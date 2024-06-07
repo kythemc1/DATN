407887810d10f1c753a18913797e6cd4d3be3af8
kythemc1 <aemanucians@gmail.com>
2024-06-07 21:13:27 +0700

    first commit

--- App/Containers/AskDetails.tsx/index.tsx
+++ App/Containers/AskDetails.tsx/index.tsx
@@ -1,15 +1,15 @@
-import React, {useState, useCallback, useEffect} from 'react';
-import {GiftedChat, InputToolbar} from 'react-native-gifted-chat';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {StyleSheet} from 'react-native';
-import {API, API_KEY_CHAT_BOT} from 'Configs/Constants/API';
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { API, API_KEY_CHAT_BOT } from "Configs/Constants/API";
 import HeaderChat from "Components/Commons/HeaderChat";
-const logo = require('../../Assets/Images/2.jpg');
+
+const logo = require("../../Assets/Images/2.jpg");
 const API_URL = API.API_CHAT_BOT;
 const YOUR_API_KEY = API_KEY_CHAT_BOT;
-const MAX_TOKENS = 1000;
 
-export default function AskDetails({navigation} : any) {
+export default function AskDetails({ navigation }: any) {
   const [messages, setMessages] = useState([] as any);
   useEffect(() => {
     firstMessage();
@@ -19,47 +19,70 @@ export default function AskDetails({navigation} : any) {
     setMessages([
       {
         _id: 1,
-        text: 'Chào bạn',
+        text: "Chào bạn",
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'Chatbot GPT',
-          avatar: logo,
-        },
-      },
+          name: "Chatbot GPT",
+          avatar: logo
+        }
+      }
     ]);
   };
 
   const onSend = useCallback((messages = []) => {
     setMessages((previousMessages: any) =>
-      GiftedChat.append(previousMessages, messages),
+      GiftedChat.append(previousMessages, messages)
     );
+    // @ts-ignore
     const value = messages[0].text;
     callApi(value);
   }, []);
 
-  const callApi = async (value: any) => {
-    const res = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${YOUR_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo-16k",
-        messages: value,
-        temperature: 1,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      }),
-    });
-    const data = await res.json();
-    if (data) {
-      console.log(data,'dataaaaaaa');
-      const value = data?.choices[0]?.text;
-      addNewMessage(value);
+  const callApi = async (value: string) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${YOUR_API_KEY}`
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo-16k",
+          messages: [
+            {
+              role: "system",
+              content: "You are a helpful assistant proficient in teaching Japanese. Your task is to assist the user in learning Japanese language effectively."
+            },
+            {
+              role: "user",
+              content: `${value}`
+            }
+          ],
+          temperature: 0.7,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        })
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Error:", errorData);
+        return;
+      }
+
+      const data = await response.json();
+      if (data) {
+        const messageValue = data?.choices[0]?.message?.content;
+        if (messageValue) {
+          addNewMessage(messageValue);
+        } else {
+          console.error("No message content found in the response.");
+        }
+      }
+    } catch (error) {
+      console.error("Request failed:", error);
     }
   };
 
@@ -70,13 +93,13 @@ export default function AskDetails({navigation} : any) {
       createdAt: new Date(),
       user: {
         _id: 2,
-        name: 'Chatbot GPT',
-        avatar: logo,
-      },
+        name: "Chatbot GPT",
+        avatar: logo
+      }
     };
 
     setMessages((previousMessages: any) =>
-      GiftedChat.append(previousMessages, value),
+      GiftedChat.append(previousMessages, value)
     );
   };
 
@@ -85,11 +108,11 @@ export default function AskDetails({navigation} : any) {
       <InputToolbar
         {...props}
         containerStyle={{
-          backgroundColor: 'white',
-          borderTopColor: '#E8E8E8',
+          backgroundColor: "white",
+          borderTopColor: "#E8E8E8",
           borderTopWidth: 1,
           borderRadius: 20,
-          borderColor: 'blue',
+          borderColor: "blue"
           // height: 50,
           // marginBottom: 20
         }}
@@ -98,8 +121,8 @@ export default function AskDetails({navigation} : any) {
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <HeaderChat navigation={navigation} screenBack={'TabNavigation'} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <HeaderChat navigation={navigation} screenBack={"TabNavigation"} />
       <GiftedChat
         messages={messages}
         showAvatarForEveryMessage={true}
@@ -107,7 +130,7 @@ export default function AskDetails({navigation} : any) {
         renderInputToolbar={props => customtInputToolbar(props)}
         user={{
           _id: 1,
-          avatar: logo,
+          avatar: logo
         }}
         keyboardShouldPersistTaps="never"
         textInputProps={styles.textInput}
@@ -117,21 +140,21 @@ export default function AskDetails({navigation} : any) {
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFFFFF', // Màu nền của InputToolbar
+    backgroundColor: "#FFFFFF" // Màu nền của InputToolbar
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 25,
-    marginHorizontal: 8,
+    marginHorizontal: 8
   },
   textInput: {
-    color: 'black', // Đặt màu chữ là đen
+    color: "black", // Đặt màu chữ là đen
     flex: 1,
     paddingHorizontal: 10,
-    fontSize: 16,
-  },
+    fontSize: 16
+  }
 });
